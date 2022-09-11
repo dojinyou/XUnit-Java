3e80554ce91f9f95fba339063afbc54ff9587155
dojinyou <djyou128@gmail.com>
2022-09-11 11:10:04 +0900

    :sparkles: run 함수 추가 및 이름을 통한 함수 호출

--- src/main/java/com/dojinyou/xuint/WasRun.java
+++ src/main/java/com/dojinyou/xuint/WasRun.java
@@ -1,9 +1,27 @@
 package com.dojinyou.xuint;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class WasRun {
+  private final String name;
   public boolean wasRun = false;
+
+  public WasRun(String name) {
+    this.name = name;
+  }
 
   public void testMethod() {
     wasRun = true;
+  }
+
+  public void run() {
+    try {
+      Method method = getClass().getMethod(name);
+      method.invoke(this);
+
+    } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
--- src/main/java/com/dojinyou/xuint/XUnitTest.java
+++ src/main/java/com/dojinyou/xuint/XUnitTest.java
@@ -2,9 +2,9 @@ package com.dojinyou.xuint;
 
 public class XUnitTest {
   public static void main(String[] args) {
-    WasRun wasRun = new WasRun();
+    WasRun wasRun = new WasRun("testMethod");
     System.out.println("isFalse(wasRun.wasRun) : " + !wasRun.wasRun); // false
-    wasRun.testMethod();
+    wasRun.run();
     System.out.println("isTrue(wasRun.wasRun) : " + wasRun.wasRun); // false
   }
 }
